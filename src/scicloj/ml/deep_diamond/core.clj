30c4a6cc367d3d813e16f1d72345a1a5278f3fea
Carsten Behring <carsten.behring@gmail.com>
2024-05-10 13:53:07 +0000

    try neanderthal

--- src/scicloj/ml/deep_diamond/core.clj
+++ src/scicloj/ml/deep_diamond/core.clj
@@ -1,8 +1,12 @@
 (ns scicloj.ml.deep-diamond.core)
 
+(set! *unchecked-math* :warn-on-boxed)
+
 (use '[uncomplicate.neanderthal core native])
+
 
 (def x (dv 1 2 3))
 (def y (dv 10 20 30))
-(dot x y)
+(println
+ (dot x y))
 
