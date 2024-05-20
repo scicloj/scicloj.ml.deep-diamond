9a55d9ffe99fe4c33cc9780efcef374e7bc2d3d3
Carsten Behring <carsten.behring@gmail.com>
2024-05-20 19:13:36 +0200

    workng test

--- test/scicloj/ml/deep_diamond/classification_test.clj
+++ test/scicloj/ml/deep_diamond/classification_test.clj
@@ -1,26 +1,47 @@
 (ns scicloj.ml.deep-diamond.classification-test
   (:require
 
+
+   [clojure.test :refer [deftest is]]
    [scicloj.ml.deep-diamond.classification]
+   [scicloj.ml.deep-diamond.boston-housing-data :as boston-data]
    [scicloj.metamorph.ml :as ml]
+   [scicloj.metamorph.ml.loss :as loss]
    [tech.v3.dataset.modelling :as ds-mod]
-   [tablecloth.api :as tc]))
+   [tablecloth.api :as tc]
+   [tablecloth.column.api :as tcc]))
 
 
-(def ds
-  (->
-   (tc/dataset {:a [1]
-                :b [1]})
-   (ds-mod/set-inference-target :b)))
+(def boston-data
+  (boston-data/boston-data))
 
 
+;; (-> boston-data :train-ds (tc/rows) first count)
+
+(def train-ds
+  (-> boston-data
+      :train-ds
+      (ds-mod/set-inference-target :y)))
+
+(frequencies
+ (-> train-ds :y))
 
 
-(run!
- (fn [_]
-   (let [model (ml/train ds {:model-type :deep-diamond/classification})
-         prediction (ml/predict ds model)]
-     (println prediction)))
+(def test-ds
+  (-> boston-data
+      :test-ds
+      (tc/drop-columns [:y])))
 
-          
- (range 50))
+
+(deftest boston-accurcay
+
+
+  (time
+   (let [model (ml/train train-ds {:model-type :deep-diamond/classification})
+         prediction (ml/predict test-ds model)]
+
+
+     (is (< 0.84
+            (loss/classification-accuracy
+             (-> boston-data :test-ds :y)
+             (tcc/round (:prediction prediction))))))))
